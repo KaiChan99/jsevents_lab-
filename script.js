40f2b88ceea67234c087162617c176c06bee5c5d
Hanqing Huang <h.hanqing@yahoo.com>
2022-09-27 14:35:36 +0100

    add to list

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 const enterBtn = document.getElementById("enter");
 const input = document.getElementById("new-todo");
-const list = document.querySelector(".list");
+const list = document.querySelector("#list");
 
 enterBtn.addEventListener('click', ()=>{
+    console.log("hello");
     let listItem = document.createElement('li'); //create list item
     listItem.innerText = input.value; //set list item equal to inputted text
     list.appendChild(listItem); //add this to the list 
+
 })
