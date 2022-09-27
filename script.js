3950ca733275d405973563b98f666f5352532787
Hanqing Huang <h.hanqing@yahoo.com>
2022-09-27 14:44:48 +0100

    delete button

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const input = document.getElementById("new-todo");
 const list = document.querySelector("#list");
 
 enterBtn.addEventListener('click', ()=>{
-    console.log("hello");
     let listItem = document.createElement('li'); //create list item
     listItem.innerText = input.value; //set list item equal to inputted text
     list.appendChild(listItem); //add this to the list 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Delete";
+    document.body.appendChild(deleteBtn);
 
 })
